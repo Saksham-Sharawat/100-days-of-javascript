9398e3f947f247071cde66c13c52036c283378a3
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-07-14 15:29:23 +0530

    Add all files of day 2

---
+++ Day 2 - Data types/day-2.js
@@ -0,0 +1,44 @@
+console.log("Types of primitive data type");
+
+
+// string
+
+let Name = "Saksham";
+console.log("Name =", Name);
+console.log("Data type of Name =", typeof Name);
+
+// number
+
+let Age = 19;
+console.log("Age =", Age);
+console.log("Data type of Age=", typeof Age);
+
+// boolean
+
+let isStudent = true;
+console.log("isStudent =", isStudent);
+console.log("Data type of isStudent =", typeof isStudent);
+
+// undefine
+
+let a;
+console.log("a =", a);
+console.log("Data type of a =", typeof a);
+
+// null
+
+let b = null;
+console.log("b =", b);
+console.log("Data type of b =", typeof b);
+
+// bigint
+
+let c = BigInt("123456789")
+console.log("c =", c);
+console.log("Data type of c =", typeof c);
+
+// symbol
+
+let d = Symbol("Hello!");
+console.log("d =", d);
+console.log("Data type of d =", typeof d);
