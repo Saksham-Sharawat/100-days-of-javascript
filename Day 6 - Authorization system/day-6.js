42c78ace543e019e95f3862e7cff7f84b9f27900
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-07-19 00:00:33 +0530

    Add all files

---
+++ Day 6 - Authorization system/day-6.js
@@ -0,0 +1,19 @@
+// Authorization system 
+
+// user data
+
+let username = "Saksham";
+let password = "123456789";
+
+// Login check
+
+let loginStatus = username === "Saksham" && password === "123456789" ? "Logn successful✅" : "Incorrect username or password❌";
+
+console.log("Username :", username);
+console.log("Password :", password);
+console.log(loginStatus);
+
+// Wellcome message
+
+let wellcome = username ?? "User";
+console.log("Wellcome,", wellcome);
