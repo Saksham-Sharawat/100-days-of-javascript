84112d4a2d75ee0ddb5f7b31cf2eeb82bb48ae74
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-07-27 21:46:43 +0530

    Initialise all files

---
+++ Day 12 - Operational calculator/day-12.js
@@ -0,0 +1,27 @@
+// Operational calculator
+
+console.log("Operational calculator");
+
+// calculation function 
+
+function add (num1,num2) {
+    return (num1 + num2);
+}
+
+function subtract (num1, num2) {
+    return (num1 - num2);
+}
+
+function multiply (num1, num2) {
+    return (num1*num2);
+}
+
+function divide (num1,num2) {
+    return (num1/num2);
+}
+
+// main calculator
+
+function calculator (operator, num1 ,num2) {
+
+}
