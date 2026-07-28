f54fe3da1d01929979ff9c53a94d8544e1f3833f
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-07-28 16:24:06 +0530

    Build the logic

--- Day 12 - Operational calculator/day-12.js
+++ Day 12 - Operational calculator/day-12.js
@@ -4,24 +4,56 @@ console.log("Operational calculator");
 
 // calculation function 
 
-function add (num1,num2) {
+function add(num1, num2) {
     return (num1 + num2);
 }
 
-function subtract (num1, num2) {
+function subtract(num1, num2) {
     return (num1 - num2);
 }
 
-function multiply (num1, num2) {
-    return (num1*num2);
+function multiply(num1, num2) {
+    return (num1 * num2);
 }
 
-function divide (num1,num2) {
-    return (num1/num2);
+function divide(num1, num2) {
+    return (num1 / num2);
 }
 
 // main calculator
 
-function calculator (operator, num1 ,num2) {
+function calculator(operation, num1, num2) {
+    return operation(num1, num2);
+}
 
+let userOperation = prompt("Enter the operation");
+let firstNum = Number(prompt("Enter the first number"));
+let secondNum = Number(prompt("Enter the second number"));
+
+let operation;
+
+switch (userOperation) {
+    case "add":
+        operation = add;
+        break;
+
+    case "subtract":
+        operation = subtract;
+        break;
+
+    case "multiply":
+        operation = multiply;
+        break;
+
+    case "divide":
+        operation = divide;
+        break;
+
+    default:
+    console.log("Invalid operation");
+}
+
+if (operation) {
+    let result = calculator(operation, firstNum, secondNum);
+    console.log(result);
 }
