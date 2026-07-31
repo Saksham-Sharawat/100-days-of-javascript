1057576998f69a3ba922826cbe18d3b66a35a5cd
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-07-31 22:22:48 +0530

    Building logic

---
+++ Day 13 - Student result system/day-13.js
@@ -0,0 +1,21 @@
+
+function passStudent (name) {
+    console.log(name, "passed.");
+}
+
+function failStudent (name) {
+    console.log(name, "failed.");
+}
+
+function distinctionStudent (name) {
+    console.log(name, "passed with distinction.")
+}
+
+function processResult (name, marks, callback) {
+    callback(name);
+
+    if (marks>=90 && marks<=100) {
+        
+    }
+        
+}
