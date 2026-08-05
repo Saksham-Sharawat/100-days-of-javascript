57f606910981a5e94124a29daecde62320d7bfb4
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-08-05 10:42:12 +0530

    Build complete logic

--- Day 14 - Reverse string/day-14.js
+++ Day 14 - Reverse string/day-14.js
@@ -1,6 +1,10 @@
 function reverseString (str) {
     if (str === "") return "";
     let nextStep = str.slice(1);
-    reverseString(nextStep);
-    
+    let reversedPart = reverseString(nextStep);
+    let firstChar = str.charAt(0);
+    let answer = (reversedPart + firstChar);
+    return answer;
 }
+
+console.log(reverseString(prompt("Enter the word")));
