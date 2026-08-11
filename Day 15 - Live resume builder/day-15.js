33b8567df007ed5484722ac21032d2d966ab2808
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-08-11 22:53:58 +0530

    Initialise basic logic

---
+++ Day 15 - Live resume builder/day-15.js
@@ -0,0 +1,19 @@
+// Live resume builder
+
+// element selector
+
+let name =  document.querySelector(".name");
+let profession = document.querySelector(".profession");
+let college = document.querySelector(".college");
+let skills = document.querySelector(".skills");
+let careerGoal = document.querySelector(".career-goal");
+let button = document.querySelector("button");
+let resume = document.querySelector(".resume");
+
+// values
+
+let nameValue = name.value;
+let professionValue = profession.value;
+let collegeValue = college.value;
+let skillsValue = skills.value;
+let careerGoalValue = careerGoal.value;
