b46b2c6d591af0b02920673f53aea91ea8875f5e
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-08-17 01:23:29 +0530

    Build the complete logic

--- Day 15 - Live resume builder/day-15.js
+++ Day 15 - Live resume builder/day-15.js
@@ -2,22 +2,41 @@
 
 // element selector
 
-let name =  document.querySelector(".name");
-let profession = document.querySelector(".profession");
-let college = document.querySelector(".college");
-let skills = document.querySelector(".skills");
-let careerGoal = document.querySelector(".career-goal");
-let button = document.querySelector("button");
-let resume = document.querySelector(".resume");
+const name =  document.querySelector(".name");
+const profession = document.querySelector(".profession");
+const college = document.querySelector(".college");
+const skills = document.querySelector(".skills");
+const careerGoal = document.querySelector(".career-goal");
+const button = document.querySelector("button");
+const resume = document.querySelector(".resume-body");
 
-// values
+button.addEventListener("click", function () {
+    
+    let nameValue = name.value;
+    let professionValue = profession.value;
+    let collegeValue = college.value;
+    let skillsValue = skills.value;
+    let careerGoalValue = careerGoal.value;
+    
+    if (
+        nameValue === "" ||
+        professionValue === "" ||
+        collegeValue === "" ||
+        skillsValue === "" ||
+        careerGoalValue === ""
+    ) {
+        resume.innerHTML = "<div class = 'error-container'>" + "<div class = 'error-msg' >" + "<p>" + "Please enter all valid information in all fields" + "</p>" + "</div>" + "</div>";
 
-let nameValue = name.value;
-let professionValue = profession.value;
-let collegeValue = college.value;
-let skillsValue = skills.value;
-let careerGoalValue = careerGoal.value;
+        return;
+    }
 
-button.addEventListener("click", function {
-    console.log("Button clicked");
-})
+    resume.innerHTML = "<p>" + "Name: " + nameValue + "</p>" + "<p>" + "Profession: " + professionValue + "</p>" + "<p>" + "College: " + collegeValue + "</p>" + "<p>" + "Skills: " + skillsValue + "</p>" + "<p>" + "Career goal: " + careerGoalValue + "</p>";
+    
+    let info = resume.querySelectorAll("p");
+
+    for (let i=0; i<info.length; i++) {
+        info[i].style.marginBottom = "0.5em"
+    }
+
+
+});
