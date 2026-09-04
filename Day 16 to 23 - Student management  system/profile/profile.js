d0d0b750db34631b1c4e5be0c5c701bf6d784fd3
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-09-04 23:06:25 +0530

    Build the profile update form

--- Day 16 to 23 - Student management  system/profile/profile.js
+++ Day 16 to 23 - Student management  system/profile/profile.js
@@ -3,3 +3,5 @@ let button = document.querySelector('.edit-button');
 button.addEventListener ('click' , function () {
     console.log ("Edit button clicked");
 })
+
+let form = document.querySelector('.edit-form');
