d6f73f2187b5e932da798abb5062634383919fa3
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-09-09 23:59:27 +0530

    Print updated name

--- Day 16 to 24 - Student management  system/profile/profile.js
+++ Day 16 to 24 - Student management  system/profile/profile.js
@@ -6,3 +6,12 @@ form.style.display = "none";
 button.addEventListener ('click' , function () {
     form.style.display = "flex";
 })
+
+let name_input = document.querySelector('#name');
+let display_name = document.querySelector('.display-name');
+let save_button = document.querySelector('.save-button');
+
+save_button.addEventListener('click' , function () {
+    let new_name = name_input.value;
+    display_name.innerHTML = new_name;
+})
