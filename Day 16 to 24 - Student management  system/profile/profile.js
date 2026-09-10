d8af299b01460b1cd188ca986a73c400f2017d05
Saksham-Sharawat <sakshamsharawat04@gmail.com>
2026-09-10 23:16:11 +0530

    Make the functionality to update the name when form is submited

--- Day 16 to 24 - Student management  system/profile/profile.js
+++ Day 16 to 24 - Student management  system/profile/profile.js
@@ -7,11 +7,19 @@ button.addEventListener ('click' , function () {
     form.style.display = "flex";
 })
 
+// name
+
 let name_input = document.querySelector('#name');
 let display_name = document.querySelector('.display-name');
-let save_button = document.querySelector('.save-button');
+let display_name_table = document.querySelector('.display-name-table');
 
-save_button.addEventListener('click' , function () {
+form.addEventListener('submit' , function (event) {
+
+    event.preventDefault();
+
     let new_name = name_input.value;
+    
     display_name.innerHTML = new_name;
+    display_name_table.innerHTML = new_name;
+
 })
